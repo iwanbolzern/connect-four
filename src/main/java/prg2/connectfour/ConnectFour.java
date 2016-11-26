dd7da5c9a2bcd4be8b24ae0886481d9f48a27528
BinaryTENSHi <binarytenshi@gmail.com>
2016-11-26 17:36:53 +0100

    Exit on close

--- src/main/java/prg2/connectfour/ConnectFour.java
+++ src/main/java/prg2/connectfour/ConnectFour.java
@@ -2,26 +2,25 @@ package prg2.connectfour;
 
 import java.awt.Dimension;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 import prg2.connectfour.ui.PlayGround;
 import prg2.connectfour.comlayer.NetworkEnv;
 
 public class ConnectFour extends JFrame {
-	
-	private NetworkEnv networkEnv;
-	
-	public ConnectFour() {
-		add(new PlayGround(10, 10));
-	}
-	
-	
-	public static void main(String[] args) {
-		JFrame.setDefaultLookAndFeelDecorated(true);
-	    JFrame mainFrame = new ConnectFour();
-	    mainFrame.setTitle("Connect four - Have fun");
-	    mainFrame.setPreferredSize(new Dimension(400, 200));
-	    mainFrame.pack();
-	    mainFrame.setVisible(true);
-	}
+    private NetworkEnv networkEnv;
+
+    private ConnectFour() {
+        add(new PlayGround(10, 10));
+    }
+
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame mainFrame = new ConnectFour();
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setTitle("Connect four - Have fun");
+        mainFrame.setPreferredSize(new Dimension(400, 200));
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
 }
