4aa974724faf0a31a9da40d3d9a9c1b6510aa6e8
David Craven <david@craven.ch>
2016-12-01 10:42:44 +0100

    wip

--- src/main/java/prg2/connectfour/ConnectFour.java
+++ src/main/java/prg2/connectfour/ConnectFour.java
@@ -13,83 +13,80 @@ import prg2.connectfour.comlayer.NetworkEnv;
 
 public class ConnectFour extends JFrame {
     private NetworkEnv networkEnv;
-    
+
     // Screens
     private HomeScreen homeScreen;
     private SearchPlayerScreen searchPlayerScreen;
     private PlayGround playGround;
 
     private ConnectFour() {
-    	initHomeScreen();
-    	add(homeScreen);
+        initHomeScreen();
+        add(homeScreen);
     }
-    
+
     private void initHomeScreen() {
-    	homeScreen = new HomeScreen();
-    	homeScreen.addPlayListener(new HomeScreen.PlayHandler() {
-			@Override
-			public void onPlayClicked(HomeScreen.GameMode mode, String playerName) {
-				if(mode == GameMode.NETWORK) {
-					initNetwork(playerName);
-					initSearchPlayerScreen(playerName);
-					remove(homeScreen);
-					add(searchPlayerScreen);
-				} else if(mode == GameMode.SINGLE) {
-					// TODO: show message box for x,y
-					initSinglePlayGround(7, 5);
-					remove(homeScreen);
-					add(playGround);
-					
-				} else if(mode == GameMode.LOAD_GAME) {
-					// TODO: implement load game
-				} else {
-					throw new IllegalArgumentException("Game mode not known");
-				}
-//				String msg = playerName + " has started a " + mode.toString();
-//		        JOptionPane.showMessageDialog(null, msg, "information",
-//		                                      JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
-    	
-    	
+        homeScreen = new HomeScreen();
+        homeScreen.addPlayListener(new HomeScreen.PlayHandler() {
+                @Override
+                public void onPlayClicked(HomeScreen.GameMode mode, String playerName) {
+                    if(mode == GameMode.NETWORK) {
+                        initNetwork(playerName);
+                        initSearchPlayerScreen(playerName);
+                        remove(homeScreen);
+                        add(searchPlayerScreen);
+                    } else if(mode == GameMode.SINGLE) {
+                        // TODO: show message box for x,y
+                        initSinglePlayGround(7, 5);
+                        remove(homeScreen);
+                        add(playGround);
+                    } else if(mode == GameMode.LOAD_GAME) {
+                        // TODO: implement load game
+                    } else {
+                        throw new IllegalArgumentException("Game mode not known");
+                    }
+                    //				String msg = playerName + " has started a " + mode.toString();
+                    //		        JOptionPane.showMessageDialog(null, msg, "information",
+                    //		                                      JOptionPane.INFORMATION_MESSAGE);
+                }
+            });
     }
-    
+
     private void initNetwork(String playerName) {
-    	this.networkEnv = new NetworkEnv();
-    	this.networkEnv.init(playerName);
+        this.networkEnv = new NetworkEnv();
+        this.networkEnv.init(playerName);
     }
-    
-    private void initSearchPlayerScreen(String playerName) {    	
-    	this.searchPlayerScreen = new SearchPlayerScreen(this.networkEnv);
-    	this.searchPlayerScreen.addStartGameListener(new SearchPlayerScreen.StartGameHandler() {
-			@Override
-			public void startGame(String gameToken, BasePlayer player, boolean isStartSend, int x, int y) {
-				if(!isStartSend) {
-					networkEnv.addStartGameListener(new NetworkEnv.StartGameHandler() {
-						@Override
-						public void startGame(int x, int y) {
-							initNetworkPlayGround(gameToken, player, x, y);
-							remove(searchPlayerScreen);
-							add(playGround);
-						}
-					});
-				} 
-				initNetworkPlayGround(gameToken, player, x, y);
-				remove(searchPlayerScreen);
-				add(playGround);
-			}
-		});
-    	this.searchPlayerScreen.init();
+
+    private void initSearchPlayerScreen(String playerName) {
+        this.searchPlayerScreen = new SearchPlayerScreen(this.networkEnv);
+        this.searchPlayerScreen.addStartGameListener(new SearchPlayerScreen.StartGameHandler() {
+                @Override
+                public void startGame(String gameToken, BasePlayer player, boolean isStartSend, int x, int y) {
+                    if(!isStartSend) {
+                        networkEnv.addStartGameListener(new NetworkEnv.StartGameHandler() {
+                                @Override
+                                public void startGame(int x, int y) {
+                                    initNetworkPlayGround(gameToken, player, x, y);
+                                    remove(searchPlayerScreen);
+                                    add(playGround);
+                                }
+                            });
+                    }
+                    initNetworkPlayGround(gameToken, player, x, y);
+                    remove(searchPlayerScreen);
+                    add(playGround);
+                }
+            });
+        this.searchPlayerScreen.init();
     }
-    
+
     private void initNetworkPlayGround(String gameToken, BasePlayer player, int x, int y) {
-    	this.playGround = new PlayGround();
-    	this.playGround.networkInit(x, y, this.networkEnv, gameToken, player);
+        this.playGround = new PlayGround();
+        this.playGround.networkInit(x, y, this.networkEnv, gameToken, player);
     }
-    
+
     private void initSinglePlayGround(int x, int y) {
-    	this.playGround = new PlayGround();
-    	this.playGround.singleInit(x, y);
+        this.playGround = new PlayGround();
+        this.playGround.singleInit(x, y);
     }
 
     public static void main(String[] args) {
--- src/main/java/prg2/connectfour/ui/SearchPlayerScreen.java
+++ src/main/java/prg2/connectfour/ui/SearchPlayerScreen.java
@@ -4,79 +4,134 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JOptionPane;
+import javax.swing.DefaultListModel;
+import javax.swing.ListSelectionModel;
 
 import prg2.connectfour.comlayer.BasePlayer;
 import prg2.connectfour.comlayer.InvitationMsg;
 import prg2.connectfour.comlayer.InvitationResponseMsg;
 import prg2.connectfour.comlayer.NetworkEnv;
+import prg2.connectfour.comlayer.NetworkEnv.*;
 
-public class SearchPlayerScreen extends JPanel {
-	
-	private NetworkEnv networkEnv;
-	private HashMap<String, BasePlayer> invitationsTokens = new HashMap<>();
-	
-	private ArrayList<StartGameHandler> startGameListeners = new ArrayList<>();
-	
-	public SearchPlayerScreen(NetworkEnv env) {
-		this.networkEnv = env;
-	}
-	
-	public void init() {
-		initComponents();
-		startSearch();
-	}
-	
-	public void unregisterAll() {
-		
-	}
-	
-	private void initComponents() {
-		// init all ui components here
-		
-		
-		// register listeners on network env
-		this.networkEnv.addNewPlayerListener(new NetworkEnv.PlayerHandler() {
-			@Override
-			public void newPlayerDetected(BasePlayer newPlayer) {
-				// TODO add player to list
-			}
-		});
-		this.networkEnv.addInvitationListener(new NetworkEnv.InvitationHandler() {
-			@Override
-			public void invitationReceived(InvitationMsg msg) {
-				// show popup on new invitation
-			}
-		});
-		this.networkEnv.addInvitationResponseListener(new NetworkEnv.InvitationResponseHandler() {
-			@Override
-			public void invitationResponseReceived(InvitationResponseMsg msg) {
-				if(invitationsTokens.containsKey(msg.getInvitationToken())) {
-					onStartGame(msg.getInvitationToken(), invitationsTokens.get(msg.getInvitationToken()));
-				}
-			}
-		});
-	}
-	
-	private void startSearch() {
-		this.networkEnv.broadcastHelloMsg();
-	}
-	
-	private void invitePlayer(BasePlayer player) {
-		String newToken = this.networkEnv.sendInvitation(player);
-		invitationsTokens.put(newToken, player);
-	}
-	
-	private void onStartGame(String gameToken, BasePlayer player) {
-		for(StartGameHandler listener : this.startGameListeners)
-			listener.startGame(gameToken, player);
-	}
-	
-	public void addStartGameListener(StartGameHandler listener) {
-		startGameListeners.add(listener);
-	}
-	
-	public interface StartGameHandler {
-		void startGame(String gameToken, BasePlayer player);
-	}
+/**
+ * SearchPlayerScreen displays a list of players to choose
+ * from. A player can be selected to send an invitation
+ * request.
+ *
+ * @author David Craven <david@craven.ch>
+ */
+public class SearchPlayerScreen extends JPanel
+    implements PlayerHandler, InvitationHandler, InvitationResponseHandler{
+
+    private NetworkEnv networkEnv;
+    private HashMap<String, BasePlayer> invitationTokens = new HashMap<>();
+
+    private ArrayList<StartGameHandler> startGameListeners = new ArrayList<>();
+
+    private JList<String> playerList;
+    private JScrollPane playerPane;
+    private DefaultListModel<String> playerListModel;
+
+    /**
+     * The constructor initializes the UI and registers event listeners
+     * for the network interface. After initialization the constructor
+     * starts a search for possible opponents.
+     */
+    public SearchPlayerScreen(NetworkEnv env) {
+        this.setLayout(new GridLayout(1, 3));
+        this.networkEnv = env;
+
+        // init all ui components here
+        this.playerListModel = new DefaultListModel<>();
+
+        this.playerListModel.addElement("player 1");
+        this.playerListModel.addElement("player 2");
+        this.playerListModel.addElement("player 3");
+        this.playerListModel.addElement("player 4");
+
+        this.playerList = new JList<>(this.playerListModel);
+        this.playerList.setLayoutOrientation(JList.VERTICAL);
+        this.playerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        this.add(playerList);
+
+        this.playerPane = new JScrollPane(this.playerList);
+        this.add(this.playerPane);
+
+        // register listeners on network env
+        this.networkEnv.addNewPlayerListener(this);
+        this.networkEnv.addInvitationListener(this);
+        this.networkEnv.addInvitationResponseListener(this);
+
+        startSearch();
+    }
+
+    /**
+     * Event handler for the newPlayerDetected event. It adds the
+     * player to the invitationTokens HashMap and inserts a new
+     * player into the UI.
+     */
+    @Override
+    public void newPlayerDetected(BasePlayer newPlayer) {
+        this.invitationTokens.put(newPlayer.getInvitationToken(), newPlayer);
+        this.playerListModel.addElement(newPlayer.getName());
+    }
+
+    /**
+     * Event handler for the invitationReceived event. It provides
+     * message dialog to accept or reject an invitation and sends
+     * an invitation response. An invitation response is also sent
+     * when rejected.
+     */
+    @Override
+    public void invitationReceived(InvitationMsg msg) {
+        String str = "Do you want to accept an invitation from " +
+            msg.getName() + " to play connect four?";
+        int result = JOptionPane.showConfirmDialog(null, str, "Invitation",
+                                                   JOptionPane.YES_NO_OPTION,
+                                                   JOptionPane.QUESTION_MESSAGE);
+        if (this.invitationTokens.containsKey(msg.getInvitationToken())) {
+            BasePlayer opponent = this.invitationTokens.get(msg.getInvitationToken());
+            this.networkEnv.sendInvitationResponse(opponent, result == 0 ? true : false);
+        }
+    }
+
+    /**
+     * Event handler for the invitationResponseReceived event. It starts
+     * a new game.
+     */
+    @Override
+    public void invitationResponseReceived(InvitationResponseMsg msg) {
+        if(this.invitationTokens.containsKey(msg.getInvitationToken())) {
+            BasePlayer opponent = this.invitationTokens.get(msg.getInvitationToken());
+            this.onStartGame(msg.getInvitationToken(), opponent);
+        }
+    }
+
+    private void startSearch() {
+        this.networkEnv.broadcastHelloMsg();
+    }
+
+    private void invitePlayer(BasePlayer player) {
+        String newToken = this.networkEnv.sendInvitation(player);
+        invitationTokens.put(newToken, player);
+    }
+
+    private void onStartGame(String gameToken, BasePlayer player) {
+        for(StartGameHandler listener : this.startGameListeners)
+            listener.startGame(gameToken, player, true, 7, 7);
+    }
+
+    public void addStartGameListener(StartGameHandler listener) {
+        startGameListeners.add(listener);
+    }
+
+    public interface StartGameHandler {
+        void startGame(String gameToken, BasePlayer player, boolean isStartSend, int x, int y);
+    }
 }
