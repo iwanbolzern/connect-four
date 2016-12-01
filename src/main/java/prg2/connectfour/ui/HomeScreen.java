15e624c2afdd9c09cddd794317c8839b497e3ac8
Iwan Bolzern <iwan.bolzern@bluewin.ch>
2016-12-01 08:28:00 +0100

    stup for comescreen done

--- src/main/java/prg2/connectfour/ui/HomeScreen.java
+++ src/main/java/prg2/connectfour/ui/HomeScreen.java
@@ -1,8 +1,40 @@
 package prg2.connectfour.ui;
 
 
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class HomeScreen extends JPanel {
-	// put in here all home screen stuff
+	private ArrayList<PlayHandler> playListeners = new ArrayList<>();
+	
+	private JButton networkButton;
+	private JButton singleButton;
+	private JButton loadGameButton;
+	
+	public HomeScreen() {
+		
+		// Place buttons here
+		
+	}
+	
+	private void onPlayClickt(GameMode mode, String playerName) {
+		for(PlayHandler listener : playListeners)
+			listener.onPlayClickt(mode, playerName);
+	}
+	
+	public void addPlayListener(PlayHandler listener) {
+		playListeners.add(listener);
+	}
+	
+	public interface PlayHandler {
+		void onPlayClickt(GameMode mode, String playerName);
+	}
+	
+	public enum GameMode {
+		NETWORK,
+		SINGLE,
+		LOAD_GAME
+	}
 }
