99e460ba06706c3946470543f884badcb33f1e3a
BinaryTENSHi <binarytenshi@gmail.com>
2016-12-19 12:11:14 +0100

    GameFactory javadoc

--- src/main/java/prg2/connectfour/logic/GameFactory.java
+++ src/main/java/prg2/connectfour/logic/GameFactory.java
@@ -2,11 +2,24 @@ package prg2.connectfour.logic;
 
 import java.util.ArrayList;
 
+/**
+ * Convenient factory to create a new game with various properties
+ *
+ * @author Marius Schuler {@literal <binarytenshi@gmail.com>}
+ */
 public class GameFactory {
+    /**
+     * Begin creation of a new {@link Game}
+     *
+     * @return Syntax
+     */
     public static GameFactorySyntax create() {
         return new GameFactorySyntax();
     }
 
+    /**
+     * GameFactory fluent syntax
+     */
     public static class GameFactorySyntax {
         private Player[] players;
         private Grid grid;
@@ -18,21 +31,44 @@ public class GameFactory {
             this.turns = new ArrayList<>();
         }
 
+        /**
+         * Adds the given players to the game
+         *
+         * @param players Players
+         * @return Syntax
+         */
         public GameFactorySyntax withPlayers(Player... players) {
             this.players = players;
             return this;
         }
 
+        /**
+         * Adds the given grid to the game
+         *
+         * @param grid Grid
+         * @return Syntax
+         */
         public GameFactorySyntax withGrid(Grid grid) {
             this.grid = grid;
             return this;
         }
 
+        /**
+         * Adds the given turns to the game
+         *
+         * @param turns Turns
+         * @return Syntax
+         */
         public GameFactorySyntax withTurns(ArrayList<Integer> turns) {
             this.turns = turns;
             return this;
         }
 
+        /**
+         * Finishes the creation of the game
+         *
+         * @return Game
+         */
         public Game finish() {
             Game game = new Game(this.players, this.grid);
             game.importTurns(this.turns);
